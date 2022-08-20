e78c66df8ee03d6f1474e51f1659aed109ce5e0e
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-08-20 19:23:27 +0530

    fix: Service error fixed (#6)

--- twitter/twitterScrape.py
+++ twitter/twitterScrape.py
@@ -3,14 +3,16 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from time import sleep
 
 PATH = "./chromedriver"
-chrome_options = Options() 
-chrome_options.add_extension("proxy.zip") 
+options = Options() 
+# chrome_options.add_extension("proxy.zip") 
+# driver = webdriver.Chrome(PATH, options=chrome_options)
 
-driver = webdriver.Chrome(PATH, options=chrome_options)
+driver = webdriver.Chrome(service=Service(PATH), options=options)
 driver.get("https://twitter.com/login")
 
 subject = "Kaiwalya Koparkar"
