ab226eebb7f533779a0df208c3ea2ebaf01cb71d
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-09-09 12:45:40 +0530

    feat: Instagram scraper complete (without proxy)

--- instagram/instaScraper.py
+++ instagram/instaScraper.py
@@ -1,5 +1,6 @@
 
 #imports here
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,9 +9,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 
+# Soax Residential proxy connection
+# PROXY = "proxy.soax.com:10000" 
+
 #specify the path to chromedriver.exe (download and save on your computer)
 PATH = "./chromedriver"
-options = Options() 
+# options = webdriver.ChromeOptions()
+# options.add_argument('proxy.soax.com'.format(PROXY))
+options = Options()
 driver = webdriver.Chrome(service=Service(PATH), options=options)
 
 #open the webpage
@@ -27,13 +33,7 @@ password.clear()
 password.send_keys("")
 
 #target the login button and click it
-button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
-
-#nadle NOT NOW
-# not_now = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
-# not_now2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
-
-import time
+button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
 
 #target the search input field
 searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Search']")))
@@ -42,20 +42,26 @@ searchbox.clear()
 #search for the hashtag cat
 keyword = "#cat"
 searchbox.send_keys(keyword)
- 
+
 # Wait for 5 seconds
-time.sleep(5)
+time.sleep(3)
 searchbox.send_keys(Keys.ENTER)
-time.sleep(5)
+time.sleep(3)
 searchbox.send_keys(Keys.ENTER)
-time.sleep(5)
+time.sleep(3)
 
 #scroll down to scrape more images
 driver.execute_script("window.scrollTo(0, 4000);")
 
 #target all images on the page
-images = driver.find_elements_by_tag_name('img')
+time.sleep(5)
+images = driver.find_elements(By.TAG_NAME,'img')
 images = [image.get_attribute('src') for image in images]
 images = images[:-2]
 
+# No of image links scraped
 print('Number of scraped images: ', len(images))
+
+# Displaying one of the scrapped links --> Can be downloaded too
+print("Displaying image: ", images[4])
+driver.get(images[4])
